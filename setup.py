cb0a5204c9fe12e40c3531c3b3dc8e495c352394
Gerard Sweeney <gerard0sweeney@gmail.com>
2014-09-09 21:20:27 +0100

    python project skeleton

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My project',
+    'author': 'gers',
+    'url': 'wherever',
+    'download_url': 'wherever',
+    'author_email': 'gers',
+    'version:': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
