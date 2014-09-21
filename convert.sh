b4c73cb64a371ae3c8cce7cee0171957268a7d40
Gerard Sweeney <gerard0sweeney@gmail.com>
2014-09-21 16:44:57 +0100

    conversion script for new projects

---
+++ convert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROJ=$1
+MODULE=$2
+
+if [ "$PROJ" == "" ] || [ "$MODULE" == "" ]; then
+    echo "Must specify project and module names"
+fi
+
+for f in `find . -name *NAME*`; do
+   mv $f `echo $f | sed -e "s/NAME/$PROJ/"`
+done
+
+for f in `find . -name *myfile*`; do
+   mv $f `echo $f | sed -e "s/myfile/$MODULE/"`
+done
+
+sed -i -e "s/NAME/$PROJ/" setup.py
+find $PROJ tests -type f -exec sed -i -e "s/NAME/$PROJ/" {} \;
+find $PROJ tests -type f -exec sed -i -e "s/myfile/$MODULE/" {} \;
+
